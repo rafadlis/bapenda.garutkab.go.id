3f080f00ecca7bd522956e6cd1d9576e503481d1
rafadlis <rahmatfadlis@gmail.com>
2025-09-28 18:20:41 +0700

    feat: initial content

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ export default function Home() {
   return (
     <article className="typography container mx-auto">
       <h1 className="max-w-prose text-center">
-        Pengurus utama Pajak Daerah <br /> kabupaten Garut
+        Pengurus Utama Pajak Daerah <br /> Kabupaten Garut
       </h1>
+      <p className="mx-auto max-w-prose text-center">
+        Badan Pendapatan Daerah (BAPENDA) Kabupaten Garut merupakan salah satu
+        instansi pemerintah daerah tingkat kabupaten yang bertugas untuk
+        mengelola Pajak Daerah tertentu dan koordinator Retribusi Daerah.
+      </p>
     </article>
   );
 }
