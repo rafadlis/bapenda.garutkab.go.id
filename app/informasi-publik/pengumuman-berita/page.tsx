dd6816d13aa6d72516b4e34c6ec6c1c81d279379
Alwi Akbar <alwiakbar09@gmail.com>
2026-01-11 16:00:47 +0700

    informasi publik

--- app/informasi-publik/pengumuman-berita/page.tsx
+++ app/informasi-publik/pengumuman-berita/page.tsx
@@ -4,7 +4,7 @@ export default function PengumumanBerita (){
     return(
         <>
         <main className="min-h-screen flex flex-col max-w-7xl mx-auto mt-20 px-6 space-y-10">
-            <HotelInvoice/>
+            
 
         </main>
         </>
